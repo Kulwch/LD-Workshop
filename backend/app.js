d97b909306bac0ab2044ef20e138470f1e1403d8
Kulwch <76691359+Kulwch@users.noreply.github.com>
2022-02-24 21:58:04 +0100

    Started setting up backend server and database.

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const helmet = require('helmet');
+
+const app = express();
+
+app.use(helmet());
+
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+module.exports = app;
